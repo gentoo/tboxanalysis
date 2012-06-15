98815c1a7ead014d2881534f18797496240d0efc
Diego Elio Pettenò <flameeyes@flameeyes.eu>
2012-06-15 10:46:58 -0700

    Store basic data about the analysis on Amazon's SimpleDB.

--- readscript.rb
+++ readscript.rb
@@ -23,12 +23,13 @@ require 'builder'
 match = Regexp.new("( .*\\*.* ERROR: .*failed|detected file collision|Tinderbox QA Warning!|QA Notice: (Pre-stripped|file does not exist|command not found|USE flag|Files built without respecting LDFLAGS|The following files)|linux_config_exists|will always overflow|called with bigger|maintainer mode detected|econf called in src_compile)")
 
 config = IniFile.new("./tboxanalysis.ini")
-bucket_name = config['aws']['bucket']
 
 s3 = AWS::S3.new(:access_key_id => config['aws']['access_key'],
                  :secret_access_key => config['aws']['secret_key'])
 
-bucket = s3.buckets[bucket_name] || s3.buckets.create(bucket_name)
+bucket_name = config['aws']['bucket']
+bucket = s3.buckets[bucket_name]
+s3.buckets.create(bucket_name) unless bucket.exists?
 
 unless bucket.objects['htmlgrep.css']
   bucket.objects.create('htmlgrep.css',
@@ -38,11 +39,20 @@ unless bucket.objects['htmlgrep.css']
                         :storage_class => :reduced_redundancy)
 end
 
+sdb = AWS::SimpleDB.new(:access_key_id => config['aws']['access_key'],
+                        :secret_access_key => config['aws']['secret_key'])
+
+domain_name = config['aws']['domain']
+domain = sdb.domains[domain_name]
+sdb.domains.create(domain_name) unless domain.exists?
+
 Archive::Tar::Minitar::Reader.open($stdin) do |input|
   input.each do |log|
+    matches = 0
+
     next unless log.file?
 
-    html_name = File.basename(log.name, ".log") + ".html"
+    log_name  = File.basename(log.name, ".log")
 
     xml_builder = Builder::XmlMarkup.new(:indent => 2)
     xml_builder.instruct!
@@ -57,20 +67,33 @@ Archive::Tar::Minitar::Reader.open($stdin) do |input|
       xml_builder.body {
         xml_builder.ol {
           log.read.split("\n").each do |line|
-            cssclass = "match" if line =~ match
-
-            xml_builder.li line, :class => cssclass
+            if line =~ match
+              xml_builder.li line, :class => "match"
+              matches += 1
+            else
+              xml_builder.li line
+            end
           end
         }
       }
     }
 
-    bucket.objects.create(html_name,
-                          :data => html_output,
-                          :acl => :public_read,
-                          # if we don't use text/html Chromium is
-                          # unable to render it with the CSS.
-                          :content_type => 'text/html',
-                          :storage_class => :reduced_redundancy)
+    html_log = bucket.objects.create(log_name + ".html",
+                                     :data => html_output,
+                                     :acl => :public_read,
+                                     # if we don't use text/html Chromium is
+                                     # unable to render it with the CSS.
+                                     :content_type => 'text/html',
+                                     :storage_class => :reduced_redundancy)
+
+    log_parts = log_name.split(':')
+    pkg_name = log_parts[0..1].join("/")
+    date_time = log_parts[2]
+
+    domain.items.create(log_name,
+                        :matches => matches,
+                        :pkg => pkg_name,
+                        :date => date_time,
+                        :public_url => html_log.public_url);
   end
 end
